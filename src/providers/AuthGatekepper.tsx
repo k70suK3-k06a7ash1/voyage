e161d1e5ae51b157ed87cc8625d80dada331dc49
2814109 <2814109k@gmail.com>
2024-04-29 03:29:50 +0900

    fix

--- src/providers/AuthGatekepper.tsx
+++ src/providers/AuthGatekepper.tsx
@@ -9,24 +9,7 @@ export function AuthGatekepper({ children }: PropsWithChildren) {
   const session = use(Effect.runPromise(getSupanaseSessionProgram));
 
   return !session ? (
-    <div style={{ display: "flex", justifyContent: "center", width: "100vw" }}>
-      {/* <div className="rounded-lg  h-full sm:h-20 py-4 my-10 flex items-center justify-center shadow-[inset_0rem_0.2rem_0.4rem_0_rgb(0,0,0,0.4)]"> */}
-      <Spinner />
-      {/* <p className="w-[70%] text-sm">
-          Stay on top of your order status, deals, new messages and more.
-          <span className="text-[#7b90fd] cursor-pointer hover:underline">
-            Enable notifications
-          </span>
-        </p> */}
-      {/* </div> */}
-      {/* <Auth
-        supabaseClient={supabase}
-        appearance={{ theme: ThemeSupa }}
-        theme="dark"
-        providers={["google"]}
-        // onlyThirdPartyProviders
-      /> */}
-    </div>
+    <Spinner />
   ) : (
     <AuthSession value={session}>{children}</AuthSession>
   );
