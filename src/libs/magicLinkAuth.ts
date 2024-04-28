ce9bfb5b014c0ffa58c1512923f22db6332777d7
2814109 <2814109k@gmail.com>
2024-04-29 03:12:49 +0900

    fix

---
+++ src/libs/magicLinkAuth.ts
@@ -0,0 +1,12 @@
+import { supabase } from "./supabase";
+
+export async function signInWithEmail() {
+  const { data, error } = await supabase.auth.signInWithOtp({
+    email: "example@email.com",
+    options: {
+      // set this to false if you do not want the user to be automatically signed up
+      shouldCreateUser: false,
+      emailRedirectTo: "https://example.com/welcome",
+    },
+  });
+}
