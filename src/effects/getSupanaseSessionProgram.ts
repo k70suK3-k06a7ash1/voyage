c4bc1c80e506c92d81d40809b97e9977e335ed36
2814109 <2814109k@gmail.com>
2024-04-29 00:35:07 +0900

    refactoring

---
+++ src/effects/getSupanaseSessionProgram.ts
@@ -0,0 +1,24 @@
+import { AuthError, Session } from "@supabase/supabase-js";
+import { supabase } from "../libs/supabase";
+import { Effect } from "effect";
+
+class ResponseError extends Error {}
+class UnknownSessionError extends Error {}
+export const getSupanaseSessionProgram: Effect.Effect<
+  Session | null,
+  ResponseError | AuthError | UnknownSessionError
+> = Effect.gen(function* (_) {
+  const response = yield* Effect.tryPromise({
+    try: () => supabase.auth.getSession(),
+    catch: () => new ResponseError(),
+  });
+
+  switch (true) {
+    case response.error instanceof AuthError:
+      return yield* Effect.fail(new AuthError("auth error"));
+    case response.data?.session === null:
+      return yield* Effect.succeed(null);
+    default:
+      return yield* Effect.succeed(response.data.session);
+  }
+});
--- src/providers/Auth.tsx
+++ src/providers/Auth.tsx
@@ -1,17 +1,13 @@
 import { PropsWithChildren, use } from "react";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
-import { Session } from "@supabase/supabase-js";
 import { supabase } from "../libs/supabase";
 import { AuthSession } from "../contexts/AuthSession";
+import { Effect } from "effect";
+import { getSupanaseSessionProgram } from "../effects/getSupanaseSessionProgram";
 
 export default function AuthProvider({ children }: PropsWithChildren) {
-  const session = use<Session | null>(
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      return session;
-    })
-  );
-  console.log(session);
+  const session = use(Effect.runPromise(getSupanaseSessionProgram));
 
   return !session ? (
     <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
