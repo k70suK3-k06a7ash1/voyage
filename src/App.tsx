5b9c95b69efa023fada1af61bec93f04f0d59345
2814109 <2814109k@gmail.com>
2024-04-29 03:29:07 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import { Podcast } from "./components/Podcast";
 import { Footer } from "./components/Footer";
 import { SiteContext } from "./contexts/SiteContenxt";
 import { AuthSession } from "./contexts/AuthSession";
+import { Spinner } from "./components/Spinner";
 
 function App() {
   const authSession = use(AuthSession);
   return (
     <SiteContext value={{ name: authSession?.user.email ?? "" }}>
-      <Suspense fallback={<>loading</>}>
+      <Suspense fallback={<></>}>
         <Podcast />
       </Suspense>
       <p>test</p>
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { AuthGatekepper } from "./providers/AuthGatekepper.tsx";
+import { Spinner } from "./components/Spinner/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Suspense fallback={<>loading</>}>
+    <Suspense fallback={<Spinner />}>
       <AuthGatekepper>
         <App />
       </AuthGatekepper>
