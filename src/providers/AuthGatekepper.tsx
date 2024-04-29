8e4cdaa3c24a906bc754b0cd170efa17a994a66c
2814109 <2814109k@gmail.com>
2024-04-29 11:46:06 +0900

    fix

--- src/providers/AuthGatekepper.tsx
+++ src/providers/AuthGatekepper.tsx
@@ -1,14 +1,16 @@
 import { PropsWithChildren, use } from "react";
 import { AuthSession } from "../contexts/AuthSession";
-import { Effect } from "effect";
+import { Effect, Match } from "effect";
 import { getSupanaseSessionProgram } from "../effects/getSupanaseSessionProgram";
+import { Session } from "@supabase/supabase-js";
 
 export function AuthGatekepper({ children }: PropsWithChildren) {
   const session = use(Effect.runPromise(getSupanaseSessionProgram));
 
-  return !session ? (
-    <>hello</>
-  ) : (
-    <AuthSession value={session}>{children}</AuthSession>
+  const match = Match.type<Session | null>().pipe(
+    Match.when(Match.null, (_) => <>hello</>),
+    Match.orElse((_) => <AuthSession value={session}>{children}</AuthSession>)
   );
+
+  return match(session);
 }
