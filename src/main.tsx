3703fcb41e10ce9996804b92a95d92c161950f3b
2814109 <2814109k@gmail.com>
2024-04-30 19:13:18 +0900

    feat: migrated to TanStack Router and generated route tree

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,27 @@
-import React, { Suspense } from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
-import { AuthGatekepper } from "./providers/AuthGatekepper.tsx";
-import { Spinner } from "./components/Spinner/index.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Suspense fallback={<Spinner />}>
+    <RouterProvider basepath="voyage" router={router} />
+    {/* <Suspense fallback={<Spinner />}>
       <AuthGatekepper>
         <App />
       </AuthGatekepper>
-    </Suspense>
+    </Suspense> */}
   </React.StrictMode>
 );
