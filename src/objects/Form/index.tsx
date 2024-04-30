f072b148e0cd2e62a68f991c8bc3bd97fb23eaaf
2814109 <2814109k@gmail.com>
2024-04-30 17:16:46 +0900

    refactor: update AuthForm component to use useActionState instead of useState.

--- src/objects/Form/index.tsx
+++ src/objects/Form/index.tsx
@@ -1,51 +1,57 @@
-import { useState } from "react";
+import { useActionState } from "react";
 import style from "./index.module.css";
 import { Spacer } from "../../components/Spacer";
+// import { supabase } from "@/libs/supabase";
+// import { AuthError } from "@supabase/supabase-js";
+
+// async function action(formData) {
+//   console.log({ formData });
+//   const { data: _, error } = await supabase.auth.signInWithOtp({
+//     email: formData.email,
+//     options: {
+//       emailRedirectTo: "http://localhost:5173/voyage",
+//     },
+//   });
+//   if (error) throw new Error(error?.message);
+
+//   return {
+//     email: "",
+//     password: "",
+//   };
+// }
+
+// type TAuth = {
+//   email: string;
+// };
+
 const AuthForm = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleInputChange = (event: any) => {
-    if (event.target.name === "email") {
-      setEmail(event.target.value);
-    } else {
-      setPassword(event.target.value);
-    }
-  };
-
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-    // Handle form submission logic here
-  };
+  const [_auth, formAction, isPending] = useActionState(
+    async (_: any, formData: FormData) => {
+      console.log(formData.get("email"));
+    },
+    undefined
+  );
 
   return (
     <div className={style.neumorphismForm}>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email:
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={handleInputChange}
-            placeholder="Enter your email"
-            className={style.neumorphismInput}
-          />
-        </label>
-        <Spacer size={4} />
-        <label>
-          Password:
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={handleInputChange}
-            placeholder="Enter your password"
-            className={style.neumorphismInput}
-          />
-        </label>
+      <form action={formAction}>
+        <label htmlFor="email">Email</label>
+        <Spacer size={2} />
+        <input
+          id="email"
+          disabled={isPending}
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+          className={style.neumorphismInput}
+        />
+
         <Spacer size={8} />
-        <button type="submit" className={style.neumorphismButton}>
+        <button
+          type="submit"
+          disabled={isPending}
+          className={style.neumorphismButton}
+        >
           Login
         </button>
       </form>
