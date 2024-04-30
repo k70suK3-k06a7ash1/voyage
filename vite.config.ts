ecb7eef2ee3310d349654e67a9b00b17ed8cd493
2814109 <2814109k@gmail.com>
2024-04-30 19:13:52 +0900

    feat(type): add new routes and configure Vite with TanStack Router Vite Plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/voyage",
 
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), TanStackRouterVite()],
 });
