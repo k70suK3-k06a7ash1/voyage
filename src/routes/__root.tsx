621e25abb2748d05566642df42042cae0477c34d
2814109 <2814109k@gmail.com>
2024-05-01 12:59:01 +0900

    refactor: remove unused import statement

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import { Spinner } from "@/components/Spinner";
-import { AuthGatekepper } from "@/providers/AuthGatekepper";
+// import { AuthGatekepper } from "@/providers/AuthGatekepper";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { Suspense } from "react";
@@ -8,11 +8,11 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <Suspense fallback={<Spinner />}>
-        <AuthGatekepper>
-          <Outlet />
-        </AuthGatekepper>
+        {/* <AuthGatekepper> */}
+        <Outlet />
+        {/* </AuthGatekepper> */}
+        <TanStackRouterDevtools />
       </Suspense>
-      <TanStackRouterDevtools />
     </>
   ),
 });
