de68581a94dd0f16bc00d44b92888082de477914
k70suK3-k06a7ash1 <49641703+k70suK3-k06a7ash1@users.noreply.github.com>
2024-05-01 03:59:36 +0000

    Deploying to gh-pages from @ k70suK3-k06a7ash1/voyage@621e25abb2748d05566642df42042cae0477c34d 🚀

--- assets/about.lazy-C5EjlUdW.js
+++
@@ -1,1 +0,0 @@
-import{c as o,j as t}from"./index-B1BYDl4x.js";const n=o("/about")({component:e});function e(){return t.jsx("div",{className:"p-2",children:"Hello from About!"})}export{n as Route};
--- assets/index.lazy-Db4lYOjn.js
+++
@@ -1,1 +0,0 @@
-import{c as o,j as e}from"./index-B1BYDl4x.js";const c=o("/")({component:t});function t(){return e.jsx("div",{className:"p-2",children:e.jsx("h3",{children:"Welcome Home!"})})}export{c as Route};
