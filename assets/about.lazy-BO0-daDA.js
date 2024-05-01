dfb9c665b4fa122710e4b37bd180bfce780e4a9f
2814109 <2814109k@gmail.com>
2024-05-01 18:59:36 +0900

    Updates

---
+++ assets/about.lazy-BO0-daDA.js
@@ -0,0 +1,1 @@
+import{c as o,j as t}from"./index-CWH5sfh3.js";const n=o("/about")({component:e});function e(){return t.jsx("div",{className:"p-2",children:"Hello from About!"})}export{n as Route};
---
+++ assets/hello.lazy-Bw4yy6ur.js
@@ -0,0 +1,1 @@
+import{c as e,j as o}from"./index-CWH5sfh3.js";const t=e("/hello")({component:()=>o.jsx("div",{children:"Hello /hello/hello!"})});export{t as Route};
