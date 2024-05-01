35720aeccef49a3521692d744c382ec9e46aedb4
2814109 <2814109k@gmail.com>
2024-05-01 18:39:07 +0900

    fix: removed unused imports and updated logout form

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,12 @@ import { Podcast } from "./components/Podcast";
 import { Footer } from "./components/Footer";
 import { SiteContext } from "./contexts/SiteContenxt";
 import { AuthSession } from "./contexts/AuthSession";
-import { logoutAction } from "./actions/logout";
+// import { logoutAction } from "./actions/logout";
 
 function App() {
   const authSession = use(AuthSession);
   return (
     <SiteContext value={{ name: authSession?.user.email ?? "" }}>
-      <form>
-        <button formAction={logoutAction}>signout</button>
-      </form>
       <Suspense fallback={<>loading</>}>
         <Podcast />
       </Suspense>
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,3 +1,4 @@
+import { logoutAction } from "@/actions/logout";
 import { Spinner } from "@/components/Spinner";
 import { AuthGatekepper } from "@/providers/AuthGatekepper";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
@@ -7,20 +8,10 @@ import { Suspense } from "react";
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
-        <Link to="/hello" className="[&.active]:font-bold">
-          Hello
-        </Link>
-      </div>
-      <hr />
       <Suspense fallback={<Spinner />}>
         <AuthGatekepper>
+          <Header />
+          <Menu />
           <Outlet />
         </AuthGatekepper>
         <TanStackRouterDevtools />
@@ -28,3 +19,23 @@ export const Route = createRootRoute({
     </>
   ),
 });
+
+const Header = () => (
+  <form>
+    <button formAction={logoutAction}>signout</button>
+  </form>
+);
+
+const Menu = () => (
+  <div className="p-2 flex gap-2">
+    <Link to="/" className="[&.active]:font-bold">
+      Home
+    </Link>{" "}
+    <Link to="/about" className="[&.active]:font-bold">
+      About
+    </Link>
+    <Link to="/hello" className="[&.active]:font-bold">
+      Hello
+    </Link>
+  </div>
+);
