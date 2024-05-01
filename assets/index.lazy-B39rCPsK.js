c00fbab0aa36bf7046ce0c8ff0a6a0f001b0ce9d
k70suK3-k06a7ash1 <49641703+k70suK3-k06a7ash1@users.noreply.github.com>
2024-05-01 07:02:12 +0000

    Deploying to gh-pages from @ k70suK3-k06a7ash1/voyage@a85f4afe09284cfe5e97e35b3003c1d4af7b8add 🚀

---
+++ assets/index.lazy-B39rCPsK.js
@@ -0,0 +1,1 @@
+import{c as o,j as e}from"./index-D0oQ36mO.js";const c=o("/")({component:t});function t(){return e.jsx("div",{className:"p-2",children:e.jsx("h3",{children:"Welcome Home!"})})}export{c as Route};
