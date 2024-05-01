8a9166f2e978dae051ad74ee0a219565c4845eb2
2814109 <2814109k@gmail.com>
2024-05-01 18:19:50 +0900

    refactor: update loginAction to use switchRedirectUrl instead of switchRedirectDomain

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,14 +1,16 @@
 import { supabase } from "@/libs/supabase";
 
-const switchRedirectDomain = (isProd: boolean) =>
-  isProd ? "https://k70suk3-k06a7ash1.github.io" : "http://localhost:5173";
+const switchRedirectUrl = (isProd: boolean) =>
+  isProd
+    ? "https://k70suk3-k06a7ash1.github.io/voyage/#/"
+    : "http://localhost:5173/voyage/";
 
 export const loginAction = async (_: unknown, formData: FormData) => {
-  console.log(`${switchRedirectDomain(import.meta.env.PROD)}/voyage`);
+  console.log(`${switchRedirectUrl(import.meta.env.PROD)}/voyage`);
   const { error } = await supabase.auth.signInWithOtp({
     email: formData?.get("email")?.toString() ?? "",
     options: {
-      emailRedirectTo: `${switchRedirectDomain(import.meta.env.PROD)}/voyage/`,
+      emailRedirectTo: `${switchRedirectUrl(import.meta.env.PROD)}`,
     },
   });
 
