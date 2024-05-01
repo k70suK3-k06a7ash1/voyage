51490e77220b907d8d17c0d0f085bde9e11aa8b6
k70suK3-k06a7ash1 <49641703+k70suK3-k06a7ash1@users.noreply.github.com>
2024-05-01 16:29:20 +0000

    Deploying to gh-pages from @ k70suK3-k06a7ash1/voyage@5bc5d6e985320efadb425a2b10ef6a993c55cf5a 🚀

---
+++ assets/about.lazy-fMM7C9es.js
@@ -0,0 +1,1 @@
+import{c as o,j as t}from"./index-C3GyoD7a.js";const n=o("/about")({component:e});function e(){return t.jsx("div",{className:"p-2",children:"Hello from About!"})}export{n as Route};
---
+++ assets/authorized.lazy-BGX3UdLe.js
@@ -0,0 +1,1 @@
+import{c as e,j as o}from"./index-C3GyoD7a.js";const i=e("/authorized")({component:()=>o.jsx("div",{children:"authorized! please confirm email"})});export{i as Route};
