a85f4afe09284cfe5e97e35b3003c1d4af7b8add
2814109 <2814109k@gmail.com>
2024-05-01 16:01:44 +0900

    feat: update route tree and root route component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,16 +1,28 @@
 import { Spinner } from "@/components/Spinner";
-// import { AuthGatekepper } from "@/providers/AuthGatekepper";
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { AuthGatekepper } from "@/providers/AuthGatekepper";
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { Suspense } from "react";
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{" "}
+        <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link>
+        <Link to="/hello" className="[&.active]:font-bold">
+          Hello
+        </Link>
+      </div>
+      <hr />
       <Suspense fallback={<Spinner />}>
-        {/* <AuthGatekepper> */}
-        <Outlet />
-        {/* </AuthGatekepper> */}
+        <AuthGatekepper>
+          <Outlet />
+        </AuthGatekepper>
         <TanStackRouterDevtools />
       </Suspense>
     </>
