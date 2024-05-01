0a3a6c1faf92c101de1dc366d416f77ffd1dadd5
2814109 <2814109k@gmail.com>
2024-05-01 13:41:46 +0900

    feat: update package.json and main.tsx to fix routing issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import {
+  RouterProvider,
+  createRouter,
+  // createMemoryHistory,
+} from "@tanstack/react-router";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
 const router = createRouter({ routeTree });
+
+// const memoryHistory = createMemoryHistory({
+//   initialEntries: ["/voyage"],
+// });
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
@@ -17,6 +25,11 @@ declare module "@tanstack/react-router" {
 }
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider basepath="voyage" router={router} />
+    <RouterProvider
+      basepath="voyage"
+      router={router}
+
+      //  history={memoryHistory}
+    />
   </React.StrictMode>
 );
