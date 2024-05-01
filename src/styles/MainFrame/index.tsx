448c10d0ef2adbe7b46c160bcc104b73a12dbc3a
2814109 <2814109k@gmail.com>
2024-05-02 03:16:19 +0900

    chore: remove unused CSS and TypeScript files

--- src/styles/MainFrame/index.tsx
+++
@@ -1,5 +0,0 @@
-import style from "./index.module.css";
-import { PropsWithChildren } from "react";
-export const MainFrame = ({ children }: PropsWithChildren) => (
-  <div className={style.container}>{children}</div>
-);
