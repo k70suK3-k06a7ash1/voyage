b175159757ce44549e30b8862f9fadeadb0db816
2814109 <2814109k@gmail.com>
2024-05-02 17:00:54 +0900

    fix: adjusted CSS and added responsive design elements

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -16,6 +16,8 @@ const isSmartPhone = () =>
 export const Route = createRootRoute({
   component: () => (
     <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
       <div className="root-container">
         <Suspense fallback={<Spinner />}>
           <AuthGatekepper>
