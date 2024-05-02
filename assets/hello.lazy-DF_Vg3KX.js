0675aee3edcd26bfe3da09e3ed3654a0209679dd
k70suK3-k06a7ash1 <49641703+k70suK3-k06a7ash1@users.noreply.github.com>
2024-05-02 05:16:23 +0000

    Deploying to gh-pages from @ k70suK3-k06a7ash1/voyage@5c34f1596b3a5063656d56156b5f570db622aaa0 🚀

--- assets/hello.lazy-DF_Vg3KX.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as o}from"./index-C4NhZA6g.js";const t=e("/hello")({component:()=>o.jsx("div",{children:"Hello /hello/hello!"})});export{t as Route};
---
+++ assets/hello.lazy-DkBW74rW.js
@@ -0,0 +1,1 @@
+import{c as e,j as o}from"./index-BxY5GqBc.js";const t=e("/hello")({component:()=>o.jsx("div",{children:"Hello /hello/hello!"})});export{t as Route};
