271aaa4a8c8e6457a97acdcb662333ac2eca31f2
k70suK3-k06a7ash1 <49641703+k70suK3-k06a7ash1@users.noreply.github.com>
2024-05-02 08:19:40 +0000

    Deploying to gh-pages from @ k70suK3-k06a7ash1/voyage@7fac5033efd60bc66b0ff17471458a0f1ca0d3d9 🚀

---
+++ assets/authorized.lazy-CPjAkDNF.js
@@ -0,0 +1,1 @@
+import{c as e,j as o}from"./index-BYAiDav_.js";const i=e("/authorized")({component:()=>o.jsx("div",{children:"authorized! please confirm email"})});export{i as Route};
---
+++ assets/hello.lazy-nkjQHVA4.js
@@ -0,0 +1,1 @@
+import{c as e,j as o}from"./index-BYAiDav_.js";const t=e("/hello")({component:()=>o.jsx("div",{children:"Hello /hello/hello!"})});export{t as Route};
