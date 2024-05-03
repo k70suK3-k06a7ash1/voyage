14d009950a5538894c4fb789a0b3507216957e64
k70suK3-k06a7ash1 <49641703+k70suK3-k06a7ash1@users.noreply.github.com>
2024-05-03 02:49:51 +0000

    Deploying to gh-pages from @ k70suK3-k06a7ash1/voyage@3af924aba17b6f9b3e9cf8754683ba61c061eed8 🚀

---
+++ assets/authorized.lazy-DWjJ7aaZ.js
@@ -0,0 +1,1 @@
+import{c as e,j as o}from"./index-DTz0IMZZ.js";const i=e("/authorized")({component:()=>o.jsx("div",{children:"authorized! please confirm email"})});export{i as Route};
---
+++ assets/hello.lazy-D6fp6MdE.js
@@ -0,0 +1,1 @@
+import{c as e,j as o}from"./index-DTz0IMZZ.js";const t=e("/hello")({component:()=>o.jsx("div",{children:"Hello /hello/hello!"})});export{t as Route};
