698f791318a517164c3f2d50c2dfa6d00c69ef1e
2814109 <2814109k@gmail.com>
2024-05-03 10:14:03 +0900

    feat: add TravelGatekeeper provider and remove VoyageGatekeeper provider

---
+++ src/providers/TravelGatekeeper.tsx
@@ -0,0 +1,22 @@
+import { supabase } from "@/libs/supabase";
+import { Effect } from "effect";
+import { PropsWithChildren, use, useMemo } from "react";
+
+class ResponseError {}
+const getTravelProgram = Effect.gen(function* (_) {
+  const response = yield* Effect.tryPromise({
+    try: () => supabase.auth.getSession(),
+    catch: () => new ResponseError(),
+  });
+  return response;
+  //   switch (response) {
+  //     case response.error instanceof Error:
+  //       return yield* Effect.fail(new Error());
+  //   }
+});
+
+export const TravelGatekeeper = ({ children }: PropsWithChildren) => {
+  const travel = use(useMemo(() => Effect.runPromise(getTravelProgram), []));
+  console.log(travel);
+  return <>{children}</>;
+};
