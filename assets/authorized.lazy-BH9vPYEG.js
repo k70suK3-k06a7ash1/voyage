59bcc60a07d3e8b8ea362d784ddaf347f00f2157
k70suK3-k06a7ash1 <49641703+k70suK3-k06a7ash1@users.noreply.github.com>
2024-05-04 07:34:42 +0000

    Deploying to gh-pages from @ k70suK3-k06a7ash1/voyage@0bae3cfa26db7c7420365adf26fd28f563b862f7 🚀

---
+++ assets/authorized.lazy-BH9vPYEG.js
@@ -0,0 +1,1 @@
+import{c as e,j as o}from"./index-Bnduso-M.js";const i=e("/authorized")({component:()=>o.jsx("div",{children:"authorized! please confirm email"})});export{i as Route};
--- assets/authorized.lazy-JJBXjWFu.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as o}from"./index-CLjd8Eq2.js";const i=e("/authorized")({component:()=>o.jsx("div",{children:"authorized! please confirm email"})});export{i as Route};
