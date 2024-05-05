012f44a6739f6de133c62162c78060176b851364
2814109 <2814109k@gmail.com>
2024-05-05 14:04:58 +0900

    refactor: update TravelForm to remove unnecessary imports and refactor code.

--- src/forms/Travel/index.tsx
+++ src/forms/Travel/index.tsx
@@ -1,14 +1,4 @@
-import {
-  JSXElementConstructor,
-  Key,
-  ReactElement,
-  ReactNode,
-  ReactPortal,
-  useActionState,
-  useOptimistic,
-  useRef,
-  useState,
-} from "react";
+import { useOptimistic, useRef, useState } from "react";
 
 const initDialogRef = (ref: HTMLDialogElement) => {
   ref.showModal();
@@ -17,126 +7,39 @@ const initDialogRef = (ref: HTMLDialogElement) => {
   };
 };
 
-function Thread({
-  messages,
-  sendMessage,
-}: {
-  messages: any;
-  sendMessage: any;
-}) {
-  const formRef = useRef<HTMLFormElement | null>(null);
-  async function formAction(formData: FormData) {
-    addOptimisticMessage(formData.get("message"));
-    formRef?.current?.reset();
-    await sendMessage(formData);
-  }
-  const [optimisticMessages, addOptimisticMessage] = useOptimistic(
-    messages,
-    (state, newMessage) => [
-      ...state,
-      {
-        text: newMessage,
-        sending: true,
-      },
-    ]
-  );
-
-  return (
-    <>
-      {optimisticMessages.map(
-        (
-          message: {
-            text:
-              | string
-              | number
-              | bigint
-              | boolean
-              | ReactElement<unknown, string | JSXElementConstructor<any>>
-              | Iterable<ReactNode>
-              | ReactPortal
-              | Promise<
-                  | string
-                  | number
-                  | bigint
-                  | boolean
-                  | ReactPortal
-                  | ReactElement<unknown, string | JSXElementConstructor<any>>
-                  | Iterable<ReactNode>
-                  | null
-                  | undefined
-                >
-              | null
-              | undefined;
-            sending: any;
-          },
-          index: Key | null | undefined
-        ) => (
-          <div key={index}>
-            {message.text}
-            {!!message.sending && <small> (Sending...)</small>}
-          </div>
-        )
-      )}
-      <form action={formAction} ref={formRef}>
-        <input type="text" name="message" placeholder="Hello!" />
-        <button type="submit">Send</button>
-      </form>
-    </>
-  );
-}
-
-type Message = { text: string; sending: false; key: number };
-
-function App() {
-  const [messages, setMessages] = useState<Message[]>([
-    { text: "Hello there!", sending: false, key: 1 },
-  ]);
-  async function sendMessage(formData: FormData) {
-    // @ts-ignore
-    const sentMessage = await deliverMessage(formData.get("message") as string);
-    // @ts-ignore
-    setMessages((messages) => [...messages, { text: sentMessage }]);
-  }
-  return <Thread messages={messages} sendMessage={sendMessage} />;
-}
-
-async function deliverMessage(message: Message) {
+const deliverTitle = async (title: string) => {
   await new Promise((res) => setTimeout(res, 1000));
-  return message;
-}
-
+  return title;
+};
 export const TravelForm = () => {
-  const [title, setTitle] = useOptimistic("");
-
-  const [response, submitAction, isPending] = useActionState(
-    async (_: unknown, formData: FormData) => {
-      console.log(formData.get("title"));
-      setTitle("update"); //formData.get("title") as string);
-      setTimeout(() => {
-        console.log("done");
-      }, 6000);
-      // const result = await updateAction(formData.get("ttile"))
-      return formData;
-      // const response = await insertTravel()
-      // const response.stateus.ok =>
-      //     orElse()
-    },
-    null,
-    "/finished"
+  const formRef = useRef<HTMLFormElement | null>(null);
+  const [title, setTitle] = useState("");
+  const [optimisticTitle, addOptimisticTitle] = useOptimistic(
+    title,
+    (currentState, optimisticValue) => currentState + optimisticValue
   );
 
-  console.log({ isPending });
+  const sendTitle = async (formData: FormData) => {
+    const sentTitle = await deliverTitle(formData.get("title") as string);
+    setTitle(() => sentTitle);
+  };
+
+  const formAction = async (formData: FormData) => {
+    addOptimisticTitle(formData.get("title"));
+    formRef.current?.reset();
+    await sendTitle(formData);
+  };
+
   return (
     <>
       <dialog ref={initDialogRef}>
-        <App />
-        <form action={submitAction}>
+        <form ref={formRef} action={formAction}>
+          optimatic : {optimisticTitle}
+          <div />
           label : {title}
           <div />
           <input name="title" type="text" />
           <button type="submit">Submit</button>
-          {isPending && <>Loading!!!!!!!!!!!</>}
-          {response && <>{String(response)}</>}
         </form>
       </dialog>
     </>
