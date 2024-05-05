ac45a0890433dfeca1aa4187e11fb08a75f50391
k70suK3-k06a7ash1 <49641703+k70suK3-k06a7ash1@users.noreply.github.com>
2024-05-05 08:04:05 +0000

    Deploying to gh-pages from @ k70suK3-k06a7ash1/voyage@7a79171f9c19a58057b83127711bb4dac65fbb81 🚀

--- assets/authorized.lazy-BByKtU7o.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as o}from"./index-Carzi0kL.js";const i=e("/authorized")({component:()=>o.jsx("div",{children:"authorized! please confirm email"})});export{i as Route};
---
+++ assets/authorized.lazy-DuI8QyVX.js
@@ -0,0 +1,1 @@
+import{c as e,j as o}from"./index-B3vq5Sjm.js";const i=e("/authorized")({component:()=>o.jsx("div",{children:"authorized! please confirm email"})});export{i as Route};
