dead7cdba1f6be8fc0aca858e3a5adb847c7be44
2814109 <2814109k@gmail.com>
2024-05-19 19:49:12 +0900

    add actor

---
+++ src/actors/authActor.ts
@@ -0,0 +1,12 @@
+import { createActor, fromPromise } from "xstate";
+const promiseLogic = fromPromise(() => {
+  return fetch("https://syntax.fm/api/shows/latest").then((data) =>
+    data.json()
+  );
+});
+
+export const promiseActor = createActor(promiseLogic);
+promiseActor.subscribe((snapshot) => {
+  console.log(snapshot);
+});
+promiseActor.start();
--- src/providers/AuthGatekeeper.tsx
+++ src/providers/AuthGatekeeper.tsx
@@ -4,12 +4,13 @@ import { Effect, Match } from "effect";
 import { getSupanaseSessionProgram } from "../effects/getSupanaseSessionProgram";
 import { Session } from "@supabase/supabase-js";
 import { LogInForm } from "../forms/LogIn";
+import { promiseActor } from "@/actors/authActor";
 
 export function AuthGatekeeper({ children }: PropsWithChildren) {
   const session = use(
     useMemo(() => Effect.runPromise(getSupanaseSessionProgram), [])
   );
-
+  promiseActor;
   const match = Match.type<Session | null>().pipe(
     Match.when(Match.null, (_) => <LogInForm />),
     Match.orElse((session) => (
